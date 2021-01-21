9bfb2ce3655d76d576a07f3aed1e744d5ed2e355
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2021-01-21 13:02:50 +0100

    add metrics server

--- autoscale.sh
+++ autoscale.sh
@@ -1,2 +1,4 @@
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
 kubectl autoscale deployment chatcloud-back --cpu-percent=50 --min=1 --max=10 
 kubectl autoscale deployment chatcloud-front --cpu-percent=80 --min=1 --max=5
